04103c6d581fe6d2f5b17b57b6f642d339e10a6f
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-14 21:37:10 +0700

    Add files via upload

---
+++ bài3/câu4-6.py
@@ -0,0 +1,9 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+ten = input("Nhập họ và tên: ").split()
+
+ho = ten[0]
+ten_rieng = ten[1]
+
+print("Họ:", ho)
+print("Tên riêng:", ten_rieng)
