4d9f1929b0b6b11dab4470d235d8cef4f61208e6
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-14 20:30:50 +0700

    Add files via upload

---
+++ bài2/câu3-7.py
@@ -0,0 +1,9 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+def checkValue(n):
+    if n % 2 == 0:
+        print("Đây là một số chẵn")
+    else:
+        print("Đây là một số lẻ")
+
+checkValue(7)
