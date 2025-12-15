ce2eca0d87c8c6dd5dd36c983ed3d6db75d39390
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-15 08:45:10 +0700

    Add files via upload

---
+++ bài3/câu4-17.py
@@ -0,0 +1,11 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+n = int(input("Nhập n: "))
+
+for i in range(1, n):
+    tong_uoc = 0
+    for j in range(1, i):
+        if i % j == 0:
+            tong_uoc += j
+    if tong_uoc > i:
+        print(i)
