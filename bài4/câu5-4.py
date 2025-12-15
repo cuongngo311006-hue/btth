746f56d640f13b31c3169127f81168c4ea6bebad
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-15 09:41:28 +0700

    Add files via upload

---
+++ bài4/câu5-4.py
@@ -0,0 +1,14 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+
+
+arr = list(range(12, 38))
+
+print("Mảng được tạo:")
+print(arr)
+
+
+arr_reverse = arr[::-1]
+
+print("Mảng đảo ngược:")
+print(arr_reverse)
