ce30d7f62260343f9c1964f9b2cf0fb667abcf51
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-15 09:26:25 +0700

    Add files via upload

---
+++ bài4/câu5-1.py
@@ -0,0 +1,18 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+
+
+def square(n):
+    return n * n
+
+def cube(n):
+    return n * n * n
+
+def average(values):
+    total = 0.0
+    for v in values:
+        total += v
+    return total / len(values)
+
+
+
