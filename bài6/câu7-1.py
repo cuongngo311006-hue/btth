db11dba781330c554fa3087f79dee7935bdb5f0a
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-16 17:03:41 +0700

    Add files via upload

---
+++ bài6/câu7-1.py
@@ -0,0 +1,13 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+input_file = open('D:/a.txt', 'r',)
+
+for line in input_file:
+    l = len(line)
+    s = ''
+    while(l>=1):
+        s = s + line[l - 1]
+        l = l - 1
+    print(s)
+
+input_file.close()
