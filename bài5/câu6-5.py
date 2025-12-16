26d5b2cd4a1b00c105a91d4e64193db08ada6cc5
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-16 16:47:04 +0700

    Add files via upload

---
+++ bài5/câu6-5.py
@@ -0,0 +1,8 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+class py_solution:
+    def reverse_words(self, s):
+        return " ".join(reversed(s.split()))
+
+
+print(py_solution().reverse_words("hello .py"))
