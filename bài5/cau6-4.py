b9ef1aa401d5bd30aceaa6243d25693d01f524ed
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-16 16:43:49 +0700

    Add files via upload

---
+++ bài5/cau6-4.py
@@ -0,0 +1,28 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+class py_solution:
+    def roman_to_int(self, s):
+        roman_val = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        int_val = 0
+
+        for i in range(len(s)):
+            if i > 0 and roman_val[s[i]] > roman_val[s[i - 1]]:
+                int_val += roman_val[s[i]] - 2 * roman_val[s[i - 1]]
+            else:
+                int_val += roman_val[s[i]]
+
+        return int_val
+
+
+# Test chương trình
+print(py_solution().roman_to_int("MMMCMLXXXVI"))
+print(py_solution().roman_to_int("C"))
