234def7aed2c354250f0fe40e723452ca8f39dbc
cuongngo311006-hue <cuongngo311006@gmail.com>
2025-12-19 11:52:09 +0700

    Add files via upload

---
+++ bài7/câu8-4.py
@@ -0,0 +1,25 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+from tkinter import *
+
+
+window = Tk()
+window.title("Welcome to LikeGeeks app")
+window.geometry('350x200')
+
+
+lbl = Label(window, text="Hello")
+lbl.grid(column=0, row=0)
+
+
+def clicked():
+    lbl.configure(text="Button was clicked !!")
+
+btn = Button(window, 
+             text="Click Me", 
+             command=clicked, 
+             bg="orange", 
+             fg="red") 
+btn.grid(column=1, row=0)
+
+window.mainloop()
